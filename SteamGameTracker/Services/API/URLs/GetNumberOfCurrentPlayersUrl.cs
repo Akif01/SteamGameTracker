e4af628652da534e73b7e99cc110f5cf9c9f470f
Akif Öztürk <66181296+Akif01@users.noreply.github.com>
2025-03-29 18:10:31 +0700

    Added GetNumberOfCurrentPlayersUrl

---
+++ SteamGameTracker/Services/API/URLs/GetNumberOfCurrentPlayersUrl.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SteamGameTracker.Services.API.URLs
+{
+    public class GetNumberOfCurrentPlayersUrl : FormattableUrlBase
+    {
+        private readonly string _appId;
+
+        public GetNumberOfCurrentPlayersUrl(string appId)
+        {
+            _appId = appId ?? throw new ArgumentNullException(appId);
+        }
+
+        public override Dictionary<string, string> ProvidePlaceHolderValueDict()
+        {
+            return new Dictionary<string, string> 
+            {
+                { "appId", _appId }, 
+            };
+        }
+
+        public override string ProvideUrlWithPlaceholders() => "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appId}";
+    }
+}
